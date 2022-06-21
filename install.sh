f0ef62625666780fc60f380c8ce253887ed4e58c
osenberg-x <osenberg@qq.com>
2022-06-21 22:15:04 +0800

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
  # @Copyright: xvsos
  # @Author: xvs
  # @Date: 2022-06-20 09:25:51
- # @LastEditTime: 2022-06-21 08:18:33
+ # @LastEditTime: 2022-06-21 22:14:20
  # @LastEditors: OsenbergQu
  # @FilePath: /xvsos-installer/install.sh
  # @Description: 
@@ -77,8 +77,18 @@ config_app() {
   # install rust
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   
-  sudo pkg install chromium
+  sudo pkg install chromium vim
   sudo pkg install droid-fonts-ttf x11-fonts/wqy
+
+  # input method
+  sudo pkg install fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt zh-fcitx5-rime zh-rime-essay
+  cp /usr/local/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart/
+  mkdir -p $home/.config/autostart
+
+  echo "#input method" >> $home/.zshrc
+  echo "export GTK_IM_MODULE=fcitx" >> $home/.zshrc
+  echo "export QT_IM_MODULE=fcitx" >> $home/.zshrc
+  echo "export XMODIFIERS=@im=fcitx" >> $home/.zshrc
 }
 
 config_pkg_source
